ffa1d151c9ae8b25208d22e01a158aa9b166bbfe
soypat <graded.sp@gmail.com>
2021-09-26 17:54:30 -0300

    add more putints

--- ascii/putint.go
+++ ascii/putint.go
@@ -23,16 +23,44 @@ func PutInt32(val int32, buf []byte) int {
 
 // PutUint32 writes a uint32 value to the end of the byte slice.
 // The function returns number of digits written. Does not modify the rest of the slice.
-func PutUint32(val uint32, uibuf []byte) int {
-	if len(uibuf) < 10 {
+func PutUint32(val uint32, buf []byte) int {
+	if len(buf) < 10 {
 		panic(ErrShortBuffer)
 	}
 	var i int
-	for i = len(uibuf) - 1; val >= 10; i-- {
+	for i = len(buf) - 1; val >= 10; i-- {
 		q := val / 10
-		uibuf[i] = byte('0' + val - q*10)
+		buf[i] = byte('0' + val - q*10)
 		val = q
 	}
-	uibuf[i] = byte('0' + val)
-	return len(uibuf) - i
+	buf[i] = byte('0' + val)
+	return len(buf) - i
+}
+
+// PutUint64 writes a uint64 value to the end of the byte slice.
+// The function returns number of digits written. Does not modify the rest of the slice.
+func PutUint64(val uint64, buf []byte) int {
+	if len(buf) < 20 {
+		panic(ErrShortBuffer)
+	}
+	var i int
+	for i = len(buf) - 1; val >= 10; i-- {
+		q := val / 10
+		buf[i] = byte('0' + val - q*10)
+		val = q
+	}
+	buf[i] = byte('0' + val)
+	return len(buf) - i
+}
+
+// PutInt64 writes a int64 value to the end of the byte slice.
+// The function returns number of digits written. Does not modify the rest of the slice.
+func PutInt64(val int32, buf []byte) int {
+	if val >= 0 {
+		return PutUint64(uint64(val), buf)
+	}
+	val *= -1
+	n := PutUint64(uint64(val), buf)
+	buf[len(buf)-n-1] = '-'
+	return n + 1
 }
